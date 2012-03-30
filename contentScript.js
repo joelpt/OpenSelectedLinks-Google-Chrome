4d9d885ef30adeeda165c0217ba7a58d0eb0c71c
Joel Thornton <github@joelpt.net>
2012-03-30 12:11:50 -0700

    Support opening links selected within IFRAMEs of the top level page.

--- contentScript.js
+++ contentScript.js
@@ -1,18 +1,35 @@
-chrome.extension.onRequest.addListener(function(command, sender, sendResponse)
-{
-    if (command == "openSelectedUrls")
-    {
+chrome.extension.onRequest.addListener(function(command, sender, sendResponse) {
+    if (command == "openSelectedUrls") {
         var urls = new Array();
         var selection = window.getSelection();
-        var hrefs = document.links;
-        for (i = 0; i < hrefs.length; i++)
-        {
-            if (selection.containsNode(hrefs[i], true))
-            {
+        var doc = window.document;
+        if (selection.type != 'Range') {
+            // Look for selection within iframes on the page instead
+            var fs = document.getElementsByTagName('iframe');
+            var f;
+            for (var i = 0; f = fs[i]; i++) {
+                try {
+                    doc = f.contentDocument;
+                    selection = doc.getSelection();
+                    if (selection.type == 'Range') {
+                        break;
+                    }
+                }
+                catch(e)
+                {
+                    continue;
+                }
+            }
+        }
+        var hrefs = doc.links;
+        for (i = 0; i < hrefs.length; i++) {
+            if (selection.containsNode(hrefs[i], true)) {
                 urls.push(hrefs[i].href);
             }
         }
-        sendResponse({"urls":urls, "fromUrl":document.location.href});
+        sendResponse({
+            "urls": urls,
+            "fromUrl": document.location.href
+        });
     }
 });
-
